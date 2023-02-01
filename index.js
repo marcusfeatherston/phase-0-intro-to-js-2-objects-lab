e1a8872299723aaafb9ce4418c96b3700b275f4b
marcus featherston <marcusovfeatherston@gmail.com>
2023-02-01 16:27:32 -0700

    grinded a bit but the employee object has been modified

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+  Name: "Brooke",
+  Address: "100 S",
+};
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+  const updatedEmployee = { ...employee };
+  updatedEmployee[key] = value;
+  return updatedEmployee;
+};
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+  const updatedEmployee = employee;
+  updatedEmployee[key] = value;
+  return updatedEmployee;
+};
+
+const deleteFromEmployeeByKey = (employee, key) => {
+  const updatedEmployee = { ...employee };
+  delete updatedEmployee[key];
+  return updatedEmployee;
+};
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+  delete employee[key];
+  return employee;
+};
+
+console.log(destructivelyDeleteFromEmployeeByKey(employee, "Address"));
